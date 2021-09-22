5c9db014ac709ece1b6a4e56151f5718aebd5d20
quonguyen191019 <quocnguyen191019@gmail.com>
2021-09-22 09:30:21 +0700

    Initial Commit

---
+++ src/main/java/Bai5.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Bai5 {
+    public static void main(String[] args){
+        String name;
+        int soluong;
+        int dongia;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap ten SP: ");
+        name = scanner.nextLine();
+        System.out.println("Nhap so luong SP: ");
+        soluong = scanner.nextInt();
+        System.out.println("Nhap don gia: ");
+        dongia = scanner.nextInt();
+        int tien = soluong * dongia;
+        System.out.println("Thanh Tien: " + tien);
+        int thuegtgt = (tien * 10) / 100;
+        System.out.println("thuegtgt: " + thuegtgt);
+
+
+
+    }
+}
--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -4,23 +4,18 @@ public class Calculator {
     public static void main(String[] args){
         int a;
         int b;
-        int c;
-        int d;
-        int e;
-        int f;
-
         Scanner scanner = new Scanner(System.in);
         System.out.println("nhap so a:");
         a = scanner.nextInt();
         System.out.println("nhap so b:");
         b= scanner.nextInt();
-        c = a + b;
+        int c = a + b;
         System.out.println("tong cua a va b = "+ c);
-        d = a - b;
+        int d = a - b;
         System.out.println("hieu cua a va b= " + d);
-        e = a*b;
+        int e = a*b;
         System.out.println("tich cua a va b= "+ e);
-        f = a/b;
+        int f = a/b;
         System.out.println("thuong cua a va b = " + f);
 
 
